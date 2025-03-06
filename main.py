5e53f82bdf86c0d324ca0599b7882694f6ce1452
Jorden Anfinson <anfins_j1@denison.edu>
2025-03-05 20:48:55 -0500

    added streamlit to list of dependencies for future use :)

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sys
 from Content import Content
-
+import streamlit as st
 
 if __name__ == "__main__":
     
-
-     content = Content(sys.argv[1])
-     print(content.get_text())
-
+     try:
+          content = Content(sys.argv[1])
+          print(content.get_text())
+          st.write(content.get_text())
+     except():
+          print("Please provide a file path")
+          st.write("Please provide a file path")
     
     
 
